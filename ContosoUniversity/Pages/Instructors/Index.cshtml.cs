b16a8b12539836023c9f8f9b58647660a9868cb3
Cooper Hanke <cooper.hanke@gmail.com>
2018-07-21 17:44:08 -0500

    done with instructors page

--- ContosoUniversity/Pages/Instructors/Index.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -19,14 +19,20 @@ namespace ContosoUniversity.Pages.Instructors
 
         public InstructorIndexData Instructor { get; set; }
         public int InstructorID { get; set; }
+        public int CourseID { get; set; }
 
-        public async Task OnGetAsync(int? id)
+        public async Task OnGetAsync(int? id, int? courseID)
         {
             Instructor = new InstructorIndexData();
             Instructor.Instructors = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
                 .Include(i => i.CourseAssignments)
                 .ThenInclude(i => i.Course)
+                .ThenInclude(i => i.Department)
+                .Include(i => i.CourseAssignments)
+                .ThenInclude(i => i.Course)
+                .ThenInclude(i => i.Enrollments)
+                .ThenInclude(i => i.Student)
                 .AsNoTracking()
                 .OrderBy(i => i.LastName)
                 .ToListAsync();
@@ -34,6 +40,16 @@ namespace ContosoUniversity.Pages.Instructors
             if (id != null)
             {
                 InstructorID = id.Value;
+                Instructor instructor = Instructor.Instructors.Where(
+                    i => i.ID == id.Value).Single();
+                Instructor.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if (courseID != null)
+            {
+                courseID = courseID.Value;
+                Instructor.Enrollments = Instructor.Courses.Where(
+                    x => x.CourseID == courseID).Single().Enrollments;
             }
         }
     }
