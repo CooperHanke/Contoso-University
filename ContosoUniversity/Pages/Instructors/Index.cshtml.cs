af964212a40f8abc85ce57b232e9cdd32849b4df
Cooper Hanke <cooper.hanke@gmail.com>
2018-07-21 16:02:33 -0500

    finished instructors page

---
+++ ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,40 @@
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using ContosoUniversity.Models.SchoolViewModels;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public InstructorIndexData Instructor { get; set; }
+        public int InstructorID { get; set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            Instructor = new InstructorIndexData();
+            Instructor.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ThenInclude(i => i.Course)
+                .AsNoTracking()
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+            }
+        }
+    }
+}
